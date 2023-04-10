d7d0676a0fdc4f806b1aa35a19a9176224473286
Charles Kang <kscscy@gmail.com>
2023-04-10 17:36:16 +0900

    basic: input example

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 // import App from './App.vue'
-import App from './components/basic/provide-inject/App.vue'
+import App from './components/basic/input-example/App.vue'
 
 createApp(App).mount('#app')
