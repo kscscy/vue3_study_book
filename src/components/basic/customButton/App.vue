7427ec795d7e5f466b07d33345cbd85c9adfdde3
Charles Kang <kscscy@gmail.com>
2023-04-10 18:51:32 +0900

    basic: custom button

---
+++ src/components/basic/customButton/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <MyButton>My Button(default)</MyButton>
+  <MyButton sm>My Button sm</MyButton>
+  <MyButton lg>My Button lg</MyButton>
+  <MyButton background-white>My Button background white</MyButton>
+  <br>
+  <MyButton pill>My Button pill</MyButton>
+  <MyButton text-red>My Button red</MyButton>
+  <MyButton @click.prevent="onClick">My Button(prevent)</MyButton>
+  <MyButton type="switch" v-model="active">toggle</MyButton>
+</template>
+
+<script>
+import MyButton from './MyButton.vue';
+export default {
+  components: {
+    MyButton
+  },
+  methods: {
+    onClick(evt) {
+      console.log('evt!', evt);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 // import App from './App.vue'
-import App from './components/basic/input-example/App.vue'
+import App from './components/basic/customButton/App.vue';
 
 createApp(App).mount('#app')
