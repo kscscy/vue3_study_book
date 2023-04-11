ae44e4cd6416777d446e21a48eda9f65e6ce424a
Charles Kang <kscscy@gmail.com>
2023-04-11 16:05:25 +0900

    baisc: move components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 // import App from './App.vue'
-import App from './components/basic/table/App.vue';
+import App from './basic/table/App.vue';
 
 createApp(App).mount('#app')
